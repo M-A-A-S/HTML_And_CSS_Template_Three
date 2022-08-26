97ec6e7076805d32213cad3f777ef372bce82944
M-A-A-S <mohammedalfatih606@gmail.com>
2022-08-26 14:28:35 +0200

    Increase Number on Scrolling Feature

--- main.js
+++ main.js
@@ -47,3 +47,28 @@ window.onscroll = function () {
     });
   }
 };
+
+// Increase Number on Scrolling
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+    if (window.scrollY >= section.offsetTop) {
+        if (!started) {
+            nums.forEach(num => startCount(num));
+        }
+        started = true;
+    }
+}
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
+}
